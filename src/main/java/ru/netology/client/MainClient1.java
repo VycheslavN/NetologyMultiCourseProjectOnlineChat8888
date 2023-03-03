0f5b2669676395cfa7463102bee406f41ff4b89d
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2023-03-03 19:47:53 +0300

    Reformat code

--- src/main/java/ru/netology/client/MainClient1.java
+++ src/main/java/ru/netology/client/MainClient1.java
@@ -4,5 +4,7 @@ public class MainClient1 {
 
     public static void main(String[] args) {
 
+        Client client1 = new Client();
+
     }
 }
--- src/main/java/ru/netology/client/MainClient2.java
+++ src/main/java/ru/netology/client/MainClient2.java
@@ -3,5 +3,7 @@ package ru.netology.client;
 public class MainClient2 {
     public static void main(String[] args) {
 
+        Client client2 = new Client();
+
     }
 }
